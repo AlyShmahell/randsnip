e6aef8c73e169c898599c47d11dd807795c8bfa4
Aly Shmahell <aly.shmahell@gmail.com>
2022-10-03 08:05:07 +0200

    init

---
+++ nddm2ddm.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def nddm2ddm(mat):
+    """
+    makes a copy of a numpy array and converts said copy to a diagonally dominant array if possible
+    """
+    n, m    = mat.shape
+    assert n == m, f"{mat} is not square"
+    maxrow  = np.amax(mat, axis=-1)
+    restrow = np.sum(mat, axis=-1) - maxrow
+    assert all(maxrow > restrow), f"no dominant diagonal found in {mat}"
+    maxind  = np.argmax(mat, axis=-1)
+    res     = mat.copy()
+    res[maxind, :] = mat
+    return res
+
+if __name__ == '__main__':
+    mat = np.array([ 
+        [4, 2, 1, 101, 1],
+        [5, 104, 3, 4 ,1],
+        [105, 5, 2, 2, 3],
+        [5, 4, 105, 4, 4],
+        [2, 4, 4, 1, 101]
+    ])
+    res = nddm2ddm(mat)
+    print(res)
